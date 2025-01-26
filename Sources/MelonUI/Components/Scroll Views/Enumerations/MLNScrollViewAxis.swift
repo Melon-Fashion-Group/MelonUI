9f73fa88966483a6c49dad642c98f53b54fac10c
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-01-26 14:55:31 +0300

    feat: remove page & tracked scroll views

---
+++ Sources/MelonUI/Components/Scroll Views/Enumerations/MLNScrollViewAxis.swift
@@ -0,0 +1,33 @@
+//
+//  MLNScrollViewAxis.swift
+//  Melon Fashion UI
+//
+//  Created by Dimka Novikov on 26.01.2025.
+//  Copyright © 2025 Melon Fashion Group. All rights reserved.
+//
+
+
+// MARK: Import section
+
+import SwiftUI
+
+
+
+// MARK: - MLNScrollViewAxis
+
+///
+///
+///
+@frozen
+public enum MLNScrollViewAxis {
+
+    ///
+    ///
+    ///
+    case horizontal
+
+    ///
+    ///
+    ///
+    case vertical
+}
