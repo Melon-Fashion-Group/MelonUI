9fbe66dd44918aac27583c609c16905264a2f877
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-04-16 11:50:07 +0300

    feat: release ver. 1.0.0

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Melon-Fashion-Group/MelonKit", from: "0.41.0")
+        .package(url: "https://github.com/Melon-Fashion-Group/MelonKit", from: "1.0.0")
     ],
     targets: [
         .target(
