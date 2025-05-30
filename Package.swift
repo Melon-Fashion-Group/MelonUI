00792f7be531feda23fd6b2d94fb7c8e8b6f0cd9
Dmitry Novikov <nvkv.dmitry@gmail.com>
2025-05-30 14:40:12 +0300

    fix: update pkg ver

--- Package.swift
+++ Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Melon-Fashion-Group/MelonKit", from: "1.11.0")
+        .package(url: "https://github.com/Melon-Fashion-Group/MelonKit", from: "1.11.1")
     ],
     targets: [
         .target(
